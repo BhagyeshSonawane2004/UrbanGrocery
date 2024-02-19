a2db7e9edc92d90ed1b65e1c2bfb071b0c0e0e22
BhagyeshSonawane2004 <134283784+BhagyeshSonawane2004@users.noreply.github.com>
2024-02-19 16:15:20 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
+// App.js
 
-import { useDispatch, useSelector } from 'react-redux'; 
-import './App.css';
-import Header from './components/Header';
-import { Outlet } from 'react-router-dom';
-import toast, { Toaster } from 'react-hot-toast';
-import { useEffect } from 'react';
-import { setDataProduct } from './redux/productSlice';
+import "./App.css";
+import Header from "./components/Header"; // Corrected import path
+import { Outlet } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import { setDataProduct } from "./redux/productSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   const dispatch = useDispatch()
@@ -13,25 +14,22 @@ function App() {
  
   useEffect(()=>{
     (async()=>{
-      const res=await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`)
-      const resData=await res.json()
-   
-     dispatch(setDataProduct(resData))
+      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`) // Corrected environment variable name
+      const resData = await res.json()
+      dispatch(setDataProduct(resData))
     })()
   },[])
-  
-  console.log(productData);
+
   return (
     <>
-     <Toaster />
-       <div>
-    <Header/>
-    <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
-     <Outlet/>
-    </main>
-    </div>
+      <Toaster />
+      <div>
+        <Header />
+        <main className="pt-16 bg-slate-100 min-h-[calc(100vh)]">
+          <Outlet />
+        </main>
+      </div>
     </>
-   
   );
 }
 
